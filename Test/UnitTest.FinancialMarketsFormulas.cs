6946404a7ad829d6e1ddb465603d3845aa3a8c8d
Scott Brettle <srbrettle@gmail.com>
2018-09-07 11:42:06 +0100

    Added unit tests for FinancialMarketsFormulas class

--- Test/UnitTest.FinancialMarketsFormulas.cs
+++ Test/UnitTest.FinancialMarketsFormulas.cs
@@ -1,21 +1,22 @@
 using srbrettle.FinancialFormulas;
-using System;
 using Xunit;
 
 namespace UnitTest_FinancialFormulas
 {
     public class UnitTest_FinancialMarketsFormulas {
 
-        [Fact(Skip = "Test under development")]        
+        [Fact]
 		public void UnitTest_CalcRateOfInflation()
         {
+            var result = FinancialMarketsFormulas.CalcRateOfInflation(1000, 2000);
+            Assert.Equal(1, result);
+        }
 
-		}
-
-        [Fact(Skip = "Test under development")]
+        [Fact]
         public void UnitTest_CalcRealRateOfReturn()
         {
-
+            var result = FinancialMarketsFormulas.CalcRealRateOfReturn(0.04m, 0.02m);
+            Assert.Equal(0.0196078431372549019607843137m, result);
         }
     }
 }
